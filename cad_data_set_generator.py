c7ac80b153669e937502563fb28fbcb70bc705aa
Oren Koriat <orenk@lacoon.com>
2018-01-29 14:26:31 +0200

    added data augmentation

--- cad_data_set_generator.py
+++ cad_data_set_generator.py
@@ -9,7 +9,7 @@ def matlab_file_to_cad(matlab_file_path):
     return spio.loadmat(matlab_file_path, squeeze_me=True).get('instance')
 
 
-def prepare_data_set(dataset_dir, batch_size, channels, limit=None, balanced=True, fuzzing_mode=False):
+def prepare_data_set(dataset_dir, batch_size, channels, limit=None, balanced=True, fuzzing_mode=False, num_of_voxels_to_augment=0):
     cad_vector =[]
     label_vector = []
     counter = 0
@@ -28,6 +28,7 @@ def prepare_data_set(dataset_dir, batch_size, channels, limit=None, balanced=Tru
                 # NEVER SET THIS FLAG TRUE unless you know what you're doing
                 l[i] = randint(0, len(labels) - 1)
             cad = matlab_file_to_cad(os.path.join(dataset_dir, label, raw_data_path))
+            fuzz_cad(cad, num_of_voxels_to_augment)
             cad = cad.reshape(cad.shape[0], cad.shape[1], cad.shape[2], channels)
             counter +=1
             cad_vector.append(cad)
@@ -48,6 +49,24 @@ def prepare_data_set_smart_wrapper(dataset_dir, batch_size, channels, limit=None
         extract(dataset_dir)
         return prepare_data_set(dataset_dir[:dataset_dir.index(tar_gz_suffix)],  batch_size, channels, limit=limit, balanced=balanced, fuzzing_mode=fuzzing_mode)
     return prepare_data_set(dataset_dir, batch_size, channels, limit=limit, balanced=balanced, fuzzing_mode=fuzzing_mode)
+
+
+def flip_bit(bit):
+    flipper = {1:0, 0:1}
+    return flipper[bit]
+
+
+def choose_coordinates(dims, vec_size):
+    coords = []
+    for _ in range(dims):
+        coords.append(randint(0, vec_size - 1))
+    return coords
+
+
+def fuzz_cad(cad, num_of_flips):
+    for _ in range(num_of_flips):
+        x, y, z = choose_coordinates(3, len(cad))
+        cad[x][y][z] = flip_bit(cad[x][y][z])
 
 
 if __name__ == "__main__":
